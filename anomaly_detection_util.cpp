ac2fe579ac778593ff8d688acfe24303bd8807b1
Dorhuri123 <dany25131@hotmail.com>
2021-10-14 09:23:02 -0700

    START

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,77 @@
+//
+// Created by dor on 10/14/21.
+//
+
+#include "anomaly_detection_util.h"
+#include <math.h>
+
+float var(float* x, int size) {
+  float temp = 0, ev = 0;
+
+  // Calc the Expected value
+  for (int i=0; i< size; i++) {
+    temp += x[i];
+  }
+  ev = (float)(1.0 / size) * temp;
+
+  // Calc the variance
+  temp = 0;
+  for (int i=0; i< size; i++) {
+    temp += pow(x[i] - ev, 2);
+  }
+  temp = (float)(1.0 / size) * temp;
+
+  return temp;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size) {
+  float e1 = 0, e2 = 0;
+
+  // Calc the Expected value
+  for (int i=0; i< size; i++) {
+    e1 += (x[i] * y[i]);
+  }
+  e1 = (float)(1.0 / size) * e1;
+
+  // Calc the Expected value
+  for (int i=0; i< size; i++) {
+    e1 += (x[i] * y[i]);
+  }
+  e1 = (float)(1.0 / size) * e1;
+}
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size) {
+  return 0;
+}
+class Line{
+ public:
+  float a,b;
+  Line():a(0),b(0){}
+  Line(float a, float b):a(a),b(b){}
+  float f(float x) {
+    return a*x+b;
+  }
+};
+
+class Point{
+ public:
+  float x,y;
+  Point(float x, float y):x(x),y(x){}
+};
+
+// performs a linear regression and return s the line equation
+Line linear_reg(Point** points, int size) {
+  Line ln;
+  return ln;
+}
+
+// returns the deviation between point p and the line equation of the points
+float dev(Point p,Point** points, int size) {
+  return 0;
+}
+// returns the deviation between point p and the line
+float dev(Point p,Line l) {
+  return 0;
+}
